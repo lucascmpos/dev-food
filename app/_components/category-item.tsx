530b4acb52e4b924edcdcfeeae0eaf9445e6af82
Lucas Campos <lucasoliveiracampos81@gmail.com>
2024-04-30 14:24:24 -0300

    style: update category item center

--- app/_components/category-item.tsx
+++ app/_components/category-item.tsx
@@ -7,7 +7,7 @@ interface CategoryItemProps {
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
   return (
-    <div className="flex flex-row items-center gap-3 rounded-full bg-white px-4 py-3 shadow-md">
+    <div className="flex flex-row items-center justify-center gap-3 rounded-full bg-white px-4 py-3 shadow-md">
       <Image
         src={category.imageUrl}
         alt={category.name}
