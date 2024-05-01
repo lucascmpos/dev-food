2c45a58d6f844ff709476567a954b92be9e91706
Lucas Campos <lucasoliveiracampos81@gmail.com>
2024-05-01 16:00:06 -0300

    feat: added search restaurants page

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div className="flex justify-between px-5 pt-6">
       <Link className="flex flex-row gap-2" href="/">
-        <h1 className="text-2xl font-extrabold text-red-500">DEV</h1>
+        <h1 className="text-2xl font-extrabold italic text-red-500">DEV</h1>
         <Image
           src="/logo.png"
           alt="Dev Foods"
