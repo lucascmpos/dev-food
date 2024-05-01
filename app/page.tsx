9b188eee2ab35ddbc746ba2278de96bd779963ca
Lucas Campos <lucasoliveiracampos81@gmail.com>
2024-05-01 15:43:29 -0300

    feat: add recommended product page

--- app/page.tsx
+++ app/page.tsx
@@ -48,9 +48,12 @@ const Home = async () => {
           <Button
             variant="ghost"
             className="h-fit p-0 text-primary hover:bg-transparent"
+            asChild
           >
-            Ver todos
-            <ChevronRightIcon size={16} />
+            <Link href="/products/recommended">
+              Ver todos
+              <ChevronRightIcon size={16} />
+            </Link>
           </Button>
         </div>
         <ProductList products={products} />
@@ -65,15 +68,16 @@ const Home = async () => {
       <div className="space-y-4 py-6 pt-6">
         <div className="flex flex-row items-center justify-between px-5">
           <h2 className="font-semibold">Restaurantes recomendados</h2>
-          <Link href="/restaurants/recommended">
-            <Button
-              variant="ghost"
-              className="h-fit p-0 text-primary hover:bg-transparent"
-            >
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+            asChild
+          >
+            <Link href="/restaurants/recommended">
               Ver todos
               <ChevronRightIcon size={16} />
-            </Button>
-          </Link>
+            </Link>
+          </Button>
         </div>
         <RestaurantList />
       </div>
