a0f4f1b50a37efd6f118042e587c5eab5daf042c
Lucas Campos <lucasoliveiracampos81@gmail.com>
2024-05-02 13:34:54 -0300

    fix: fixing total price

--- app/_context/cart.tsx
+++ app/_context/cart.tsx
@@ -69,11 +69,12 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
     return (
       products.reduce((acc, product) => {
         return acc + calculateProductTotalPrice(product) * product.quantity;
-      }, 0) - Number(products?.[0]?.restaurant?.deliveryFee)
+      }, 0) + Number(products?.[0]?.restaurant?.deliveryFee)
     );
   }, [products]);
 
-  const totalDiscounts = subtotalPrice - totalPrice;
+  const totalDiscounts =
+    subtotalPrice - totalPrice + Number(products?.[0]?.restaurant?.deliveryFee);
 
   const increaseProductQuantity = (productId: string) => {
     return setProducts((prev) =>
