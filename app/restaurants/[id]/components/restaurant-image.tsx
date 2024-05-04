56007f5d8cd6f58395264b7d84f4e1cc7f4fb309
Lucas Campos <lucasoliveiracampos81@gmail.com>
2024-05-04 13:12:57 -0300

    fix: fixing user can favorite restaurants

--- app/restaurants/[id]/components/restaurant-image.tsx
+++ app/restaurants/[id]/components/restaurant-image.tsx
@@ -1,22 +1,42 @@
 "use client";
 
 import { Button } from "@/app/_components/ui/button";
-import { Restaurant } from "@prisma/client";
+import { isRestaurantFavorited } from "@/app/_helpers/restaurant";
+import useToggleFavoriteRestaurant from "@/app/_hooks/use-toggle-favorite-restaurant";
+import { Restaurant, UserFavoriteRestaurant } from "@prisma/client";
 import { ChevronLeftIcon, HeartIcon } from "lucide-react";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 interface RestaurantImageProps {
-  restaurant: Pick<Restaurant, "name" | "imageUrl">;
+  restaurant: Pick<Restaurant, "id" | "name" | "imageUrl">;
+  userFavoriteRestaurants: UserFavoriteRestaurant[];
 }
 
-const RestaurantImage = ({ restaurant }: RestaurantImageProps) => {
+const RestaurantImage = ({
+  restaurant,
+  userFavoriteRestaurants,
+}: RestaurantImageProps) => {
+  const { data } = useSession();
+
   const router = useRouter();
+
+  const isFavorite = isRestaurantFavorited(
+    restaurant.id,
+    userFavoriteRestaurants,
+  );
+
+  const { handleFavoriteClick } = useToggleFavoriteRestaurant({
+    restaurantId: restaurant.id,
+    userId: data?.user.id,
+    restaurantIsFavorited: isFavorite,
+  });
 
   const handleBackClick = () => router.back();
 
   return (
-    <div className="relative h-[215px] w-full">
+    <div className="relative h-[250px] w-full">
       <Image
         src={restaurant.imageUrl}
         alt={restaurant.name}
@@ -31,11 +51,13 @@ const RestaurantImage = ({ restaurant }: RestaurantImageProps) => {
       >
         <ChevronLeftIcon />
       </Button>
+
       <Button
         size="icon"
-        className="absolute right-4 top-4  rounded-full bg-gray-700"
+        className={`absolute right-4 top-4 rounded-full bg-gray-700 ${isFavorite && "bg-primary hover:bg-gray-700"}`}
+        onClick={handleFavoriteClick}
       >
-        <HeartIcon className="fill-white" size={20} />
+        <HeartIcon size={20} className="fill-white" />
       </Button>
     </div>
   );
