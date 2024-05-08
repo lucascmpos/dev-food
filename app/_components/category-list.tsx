8b87cb13d7b9849b07cfd6683cbbc3023bbfec8b
Lucas Campos <lucasoliveiracampos81@gmail.com>
2024-05-08 16:05:26 -0300

    style: adding horizontal padding  to category list

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -5,7 +5,7 @@ const CategoryList = async () => {
   const categories = await db.category.findMany({});
 
   return (
-    <div className="flex w-full flex-row items-center gap-3 overflow-x-scroll py-2 md:grid-cols-3 lg:flex lg:flex-row lg:items-center lg:justify-center [&::-webkit-scrollbar]:hidden">
+    <div className="flex w-full flex-row items-center gap-3 overflow-x-scroll px-2 py-2 md:grid-cols-3 lg:flex lg:flex-row lg:items-center lg:justify-center [&::-webkit-scrollbar]:hidden">
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
